72ed36adf25011f9fc1f274fe3cdf42888a10388
Stanislav <solarcoaster.st@gmail.com>
2024-01-17 11:50:12 +0200

    fixed position rule for progress bar

--- src/components/UI/progress-bar/progress-bar.styles.js
+++ src/components/UI/progress-bar/progress-bar.styles.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const MotionProgressBar = styled(motion.div)`
-  position: fixed;
+  position: sticky;
   top: 0;
   left: 0;
   right: 0;
