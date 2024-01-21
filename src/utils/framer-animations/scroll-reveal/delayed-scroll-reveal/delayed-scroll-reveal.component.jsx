e48e235ad232cb3f9fc4041f758acf61d7081ebb
Stanislav <solarcoaster.st@gmail.com>
2024-01-21 13:19:02 +0200

    modified delayed scroll reveal animation a bit

--- src/utils/framer-animations/scroll-reveal/delayed-scroll-reveal/delayed-scroll-reveal.component.jsx
+++ src/utils/framer-animations/scroll-reveal/delayed-scroll-reveal/delayed-scroll-reveal.component.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const DelayedScrollReveal = ({ index, children, speed = 2, ...props }) => {
+  const animation = {
+    initial: { opacity: 0, scale: 0.5 },
+    enter: (i) => ({
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 1,
+        delay: (index * 0.25) / speed,
+      },
+    }),
+  };
+
+  const { ref, inView, entry } = useInView({
+    threshold: 0.75,
+    triggerOnce: true,
+  });
+
   return (
     <motion.div
       {...props}
-      initial={{
-        opacity: 0,
-        scale: 0.5,
-      }}
-      whileInView={{
-        opacity: 1,
-        scale: 1,
-        transition: {
-          duration: 1,
-          delay: (index * 0.25) / speed,
-        },
-      }}
-      viewport={{ once: true }}
+      ref={ref}
+      variants={animation}
+      initial="initial"
+      animate={inView ? "enter" : ""}
     >
       {children}
     </motion.div>
